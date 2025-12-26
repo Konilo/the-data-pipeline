bfa4140655f5a9e0cd3e5a5d22ee2867356c369e
Konilo Zio (wooclap-pc) <konilo.zio@wooclap.com>
2025-12-26 14:00:47 +0000

    feat: add mermaid charts; improve contents

--- the-data-pipeline/astro.config.mjs
+++ the-data-pipeline/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,12 +16,22 @@ export default defineConfig({
 
   site: 'https://Konilo.github.io',
   base: '/the-data-pipeline',
-  
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid'],
+    },
+    rehypePlugins: [
+      [rehypeMermaid, { strategy: "img-svg", dark: true }]
+    ]
+  },
 });
